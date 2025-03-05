3d835c55453009e90bbb825f9c12d4a656239c9c
Stephen Ins <stephen.insixiengmay@gmail.com>
2025-03-05 22:19:32 +0100

    version lien header et footer vers les pages

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class AppController extends AbstractController
 {
@@ -12,12 +12,34 @@ final class AppController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('app/index.html.twig', []);
+        return $this->render('app/index.html.twig');
     }
 
+    // Route pour la page about
+    #[Route('/about', name: 'app_about')]
+    public function appAbout(): Response
+    {
+        return $this->render('app/about.html.twig');
+    }
+
+    // Route pour la page products
     #[Route('/products', name: 'app_products')]
     public function appProducts(): Response
     {
-        return $this->render('app/products.html.twig', []);
+        return $this->render('app/products.html.twig');
+    }
+
+    // Route pour la page why
+    #[Route('/why', name: 'app_why')]
+    public function appWhy(): Response
+    {
+        return $this->render('app/why.html.twig');
+    }
+
+    // Route pour la page testimonial
+    #[Route('/testimonial', name: 'app_testimonial')]
+    public function appTestimonial(): Response
+    {
+        return $this->render('app/testimonial.html.twig');
     }
 }
